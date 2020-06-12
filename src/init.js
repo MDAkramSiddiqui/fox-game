66c9d61c7bbf028bd4cf0e9100a4d4b6a72395bb
MDAkramSiddiqui <you@example.com>
2020-06-12 23:33:54 +0530

    Initial Commit - Complete Game Full Working

---
+++ src/init.js
@@ -0,0 +1,22 @@
+import gameState, { handleUserAction } from "./gameState";
+import { TICK_RATE } from "./constants";
+import initButtons from "./button";
+
+async function init() {
+  console.log("starting game");
+  initButtons(handleUserAction);
+
+  let nextTimeToTick = Date.now();
+  function nextAnimationFrame() {
+    const now = Date.now();
+    if (nextTimeToTick <= now) {
+      gameState.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  nextAnimationFrame();
+}
+
+init();
